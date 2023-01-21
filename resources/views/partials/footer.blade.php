fc7b701060f4be84c75ae8f503a55ec6fc030b98
Muhammad Rofiul Anam <rofiulanam10@gmail.com>
2023-01-21 21:38:56 +0700

    update footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,7 +1,67 @@
-<footer class="bg-light text-center text-lg-start mt-auto">
-    <!-- Copyright -->
-    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.4);">
-      © 2023 Copyright: KPH Kendal
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-white text-muted">
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container py-5 text-center text-md-start">
+      <!-- Grid row -->
+      <div class="row gap-md-5">
+
+        <!-- Grid column -->
+        <div class="col-lg mb-5 mb-md-0 text-center text-lg-start">
+          <!-- Content -->
+          <h6>
+            <img src="/img/perhutani.png" class="align-items-center"></img>
+          </h6>
+          <div class="desc">
+          Perhutani adalah Badan Usaha Milik Negara berbentuk Perusahaan Umum (Perum)
+          yang memiliki tugas dan wewenang untuk mengelola sumberdaya hutan negara
+          di pulau Jawa dan Madura. Peran strategis Perhutani adalah mendukung sistem
+          kelestarian lingkungan, sistem sosial budaya dan sistem perekonomian masyarakat perhutanan
+          </div>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-md col-lg-auto mb-5 mb-md-0">
+          <!-- Links -->
+          <h5 class="text-uppercase fw-bold mb-4"> Sosial Media </h5>
+          <p>
+            <i class="bi bi-instagram"></i> <a href="https://www.instagram.com/perumperhutani/?hl=id" target="_blank" class="text-reset">Instagram </a>
+          </p>
+          <p>
+            <i class="bi bi-twitter"></i> <a href="https://twitter.com/perumperhutani" target="_blank" class="text-reset">Twitter</a>
+          </p>
+          <p>
+            <i class="bi bi-facebook"></i> <a href="https://www.facebook.com/perumperhutani/" target="_blank" class="text-reset">Facebook</a>
+          </p>
+          <p>
+            <i class="bi bi-tiktok"></i> <a href="https://www.tiktok.com/@perum.perhutani" target="_blank" class="text-reset">Tiktok</a>
+          </p>
+          <p>
+            <i class="bi bi-youtube"></i> <a href="https://www.youtube.com/channel/UCmMvplsIxDZLQeusTWlLE2w" target="_blank" class="text-reset">Youtube </a>
+          </p>
+        </div>
+
+        <!-- Grid column -->
+        <div class="col-md col-lg-auto mb-5 mb-md-0">
+          <!-- Links -->
+          <h5 class="text-uppercase fw-bold mb-4">  Hubungi Kami </h5>
+          <p> <i class="bi bi-map"></i> Jl. Soekarno Hatta No. 322 Kendal </p>
+          <p> <i class="bi bi-envelope"></i> perhutanikphkendal@gmail.com </p>
+          <p> <i class="bi bi-telephone"></i> Phone: 0294381350 </p>
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
     </div>
-    <!-- Copyright -->
-  </footer>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-light text-center p-4" style="background-color: #006342;"> © 2023 Copyright:
+    <b class="text-light" > Perhutani KPH Kendal</b>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
