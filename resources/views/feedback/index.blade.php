9200f41a556653e984680f3f4687d405371e6698
Muhammad Rofiul Anam <rofiulanam10@gmail.com>
2023-01-22 21:04:00 +0700

    update halaman tentang sementara gini

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -1,19 +1,47 @@
 @extends("layouts.main")
 @section("container")
-<div class="row justify-content-center">
-  <div class="col-lg-6">
-      <main class="form-registration">
-          <h1 class="h3 mb-3 fw-normal text-center">Tentang</h1>
+<div class="row justify-content-center pt-5">
 
-          @if(session()->has('success'))
-            <div class="alert alert-success alert-dismissible fade show" role="alert">
-              {{ session('success') }}
-              <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-            </div>
-          @endif
+          <h1 class="h3 mb-3 fw-normal text-center pt-4">Tentang</h1>
+          <div>
+        <h2 class="center">Tentang Perhutani Kendal</h2>
 
-          
-      </main>
-  </div>
+        <ul class="justify list-unstyled">
+          <li class="p fz"> Perum Perhutani KPH Kendal merupakan Badan Usaha Milik Negara (PP Nomor 72 Tahun 2010), bagian dari Divisi Regional Jawa Tengah, dengan luas 20.300,59 ha dimana wilayah kerjanya berada di Kabupaten Kendal, Kabupaten Batang dan Kota Semarang.
+          </li><br />
+          <li class="p fz"> Batas Wilayah Administratif KPH Kendal adalah sebagai berikut:
+            <li class="p fz">  ->	Sebelah Utara		: Laut Jawa
+            <li class="p fz">	 -> Sebelah Timur		: Wilayah Kodya Semarang (KPH Semarang)
+            <li class="p fz">  ->	Sebelah Selatan	: Wilayah Kabupaten Temanggung (KPH Kedu Utara)
+            <li class="p fz">  -> Sebelah Barat		: Wilayah Kabupaten Batang (KPH Pekalongan Timur)
+          </li><br />
+          <li class="p fz"> Koordinat KPH Kendal berada di antara 6°51’ 22’’ LS – 7°7’ 17’’LS dan 109°43’ 28’’BT  – 110°24’ 35’’ BT. Status tata guna lahan hutan KPH Kendal adalah Hutan Produksi Tetap berdasarkan BATB (Berita Acara Tata Batas) tahun 1926 – 1927 dan BATB tahun 1937. Kawasan hutan KPH Kendal dikelilingi oleh tanah milik (lahan pertanian dan pemukiman) dan tanah negara (lahan perkebunan dan bekas perkebunan).
+          </li><br />
+          <li class="p fz"> Dalam pengelolaannya, KPH (Kesatuan Pemangkuan Hutan) dibagi menjadi 6 BKPH (Bagian Kesatuan Pemangkuan Hutan) dan dibagi lagi menjadi 23 RPH (Resort Pemangkuan Hutan). Pembagian 6 BKPH terdiri atas : 
+            <li class="p fz">  ->	BKPH Mangka
+            <li class="p fz">	 -> BKPH Boja 
+            <li class="p fz">  ->	BKPH Kalibodri
+            <li class="p fz">  -> BKPH Sojomerto
+            <li class="p fz">  -> BKPH Plelen
+            <li class="p fz">  -> BKPH Subah
+            </li><br />
+          <li class="p fz"> Luas kawasan hutan KPH Kendal jika dibagi berdasarkan fungsi kawasan terbagi menjadi 3 kawasan yakni : 
+            <li class="p fz">  ->	Kawasan untuk produksi 	       	: 15.877,56 ha (77%)
+            <li class="p fz">	 -> Kawasan perlindungan 		        : 2.688,57 ha (13%)
+            <li class="p fz">  ->	Kawasan untuk penggunaan lain 	: 2.137,15 ha (10%)
+          </li><br />
+          <li class="p fz"> Pengelolaan Hutan di KPH Kendal meliputi kegiatan: 
+            <li class="p fz">  ->	Tata hutan dan penyusunan rencana pengelolaan hutan
+            <li class="p fz">	 -> Pemanfaatan hutan
+            <li class="p fz">  ->	Rehabilitasi dan reklamasi hutan
+            <li class="p fz">  ->	Perlindungan hutan dan konservasi alam
+            </li><br />
+          <li class="p fz"> Pelaksanaan pekerjaan lapangan di KPH Kendal dari hulu ke hilir meliputi : Perencanaan SDH (Sumber Daya Hutan), Persemaian, Pemeliharaan Tanaman, Perawatan Hutan, Teresan, Pengelolaan Lingkungan, Tebangan hingga Angkutan Tebangan ke TPK (Tempat Penampungan Kayu).Pendapatan KPH Kendal berasal dari penjualan kayu (jati dan rimba), produksi non kayu, kerjasama kawasan hutan (di dalam kawasan hutan dan di luar kawasan hutan), wisata dan agroforestry (jagung, kopi, pisang, jengkol dan lain-lain).
+
+          </li><br />
+        </ul>
+        <!--Grid column-->
+      </div>
+
 </div>
 @endsection
