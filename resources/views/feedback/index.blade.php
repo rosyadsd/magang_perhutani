09afa44f85a81c147f0b870a298f814b4e680676
Muhammad Rofiul Anam <rofiulanam10@gmail.com>
2023-01-23 22:47:43 +0700

    update sementara lagi

--- resources/views/feedback/index.blade.php
+++ resources/views/feedback/index.blade.php
@@ -2,46 +2,45 @@
 @section("container")
 <div class="row justify-content-center pt-5">
 
-          <h1 class="h3 mb-3 fw-normal text-center pt-4">Tentang</h1>
-          <div>
-        <h2 class="center">Tentang Perhutani Kendal</h2>
+  <h3 class=" mb-3 fw-semibold text-center pt-4">Tentang Perhutani Kendal</h3>
+    <div>
+      <ul class="justify list-unstyled">
+        <li class="p fz"> Perum Perhutani KPH (Kesatuan Pemangkuan Hutan) Kendal merupakan Badan Usaha Milik Negara (PP Nomor 72 Tahun 2010), 
+          bagian dari Divisi Regional Jawa Tengah, dengan luas 20.300,59 hektar di mana wilayah kerjanya berada di Kabupaten Kendal, 
+          Kabupaten Batang dan Kota Semarang. Koordinat KPH Kendal berada di antara 6°51’ 22’’ LS – 7°7’ 17’’LS dan 
+          109°43’ 28’’BT  – 110°24’ 35’’ BT. Status tata guna lahan hutan KPH Kendal adalah Hutan Produksi Tetap berdasarkan 
+          BATB (Berita Acara Tata Batas) tahun 1926 – 1927 dan BATB tahun 1937. Kawasan hutan KPH Kendal dikelilingi oleh tanah 
+          milik (lahan pertanian dan pemukiman) dan tanah negara (lahan perkebunan dan bekas perkebunan).
+        </li><br />
 
-        <ul class="justify list-unstyled">
-          <li class="p fz"> Perum Perhutani KPH Kendal merupakan Badan Usaha Milik Negara (PP Nomor 72 Tahun 2010), bagian dari Divisi Regional Jawa Tengah, dengan luas 20.300,59 ha dimana wilayah kerjanya berada di Kabupaten Kendal, Kabupaten Batang dan Kota Semarang.
-          </li><br />
-          <li class="p fz"> Batas Wilayah Administratif KPH Kendal adalah sebagai berikut:
-            <li class="p fz">  ->	Sebelah Utara		: Laut Jawa
-            <li class="p fz">	 -> Sebelah Timur		: Wilayah Kodya Semarang (KPH Semarang)
-            <li class="p fz">  ->	Sebelah Selatan	: Wilayah Kabupaten Temanggung (KPH Kedu Utara)
-            <li class="p fz">  -> Sebelah Barat		: Wilayah Kabupaten Batang (KPH Pekalongan Timur)
-          </li><br />
-          <li class="p fz"> Koordinat KPH Kendal berada di antara 6°51’ 22’’ LS – 7°7’ 17’’LS dan 109°43’ 28’’BT  – 110°24’ 35’’ BT. Status tata guna lahan hutan KPH Kendal adalah Hutan Produksi Tetap berdasarkan BATB (Berita Acara Tata Batas) tahun 1926 – 1927 dan BATB tahun 1937. Kawasan hutan KPH Kendal dikelilingi oleh tanah milik (lahan pertanian dan pemukiman) dan tanah negara (lahan perkebunan dan bekas perkebunan).
-          </li><br />
-          <li class="p fz"> Dalam pengelolaannya, KPH (Kesatuan Pemangkuan Hutan) dibagi menjadi 6 BKPH (Bagian Kesatuan Pemangkuan Hutan) dan dibagi lagi menjadi 23 RPH (Resort Pemangkuan Hutan). Pembagian 6 BKPH terdiri atas : 
-            <li class="p fz">  ->	BKPH Mangka
-            <li class="p fz">	 -> BKPH Boja 
-            <li class="p fz">  ->	BKPH Kalibodri
-            <li class="p fz">  -> BKPH Sojomerto
-            <li class="p fz">  -> BKPH Plelen
-            <li class="p fz">  -> BKPH Subah
-            </li><br />
-          <li class="p fz"> Luas kawasan hutan KPH Kendal jika dibagi berdasarkan fungsi kawasan terbagi menjadi 3 kawasan yakni : 
-            <li class="p fz">  ->	Kawasan untuk produksi 	       	: 15.877,56 ha (77%)
-            <li class="p fz">	 -> Kawasan perlindungan 		        : 2.688,57 ha (13%)
-            <li class="p fz">  ->	Kawasan untuk penggunaan lain 	: 2.137,15 ha (10%)
-          </li><br />
-          <li class="p fz"> Pengelolaan Hutan di KPH Kendal meliputi kegiatan: 
-            <li class="p fz">  ->	Tata hutan dan penyusunan rencana pengelolaan hutan
-            <li class="p fz">	 -> Pemanfaatan hutan
-            <li class="p fz">  ->	Rehabilitasi dan reklamasi hutan
-            <li class="p fz">  ->	Perlindungan hutan dan konservasi alam
-            </li><br />
-          <li class="p fz"> Pelaksanaan pekerjaan lapangan di KPH Kendal dari hulu ke hilir meliputi : Perencanaan SDH (Sumber Daya Hutan), Persemaian, Pemeliharaan Tanaman, Perawatan Hutan, Teresan, Pengelolaan Lingkungan, Tebangan hingga Angkutan Tebangan ke TPK (Tempat Penampungan Kayu).Pendapatan KPH Kendal berasal dari penjualan kayu (jati dan rimba), produksi non kayu, kerjasama kawasan hutan (di dalam kawasan hutan dan di luar kawasan hutan), wisata dan agroforestry (jagung, kopi, pisang, jengkol dan lain-lain).
+        <li class="p fz"> Pelaksanaan pekerjaan lapangan di KPH Kendal dari hulu ke hilir meliputi
+          Perencanaan SDH (Sumber Daya Hutan), Persemaian, Pemeliharaan Tanaman, Perawatan Hutan, Teresan, Pengelolaan Lingkungan, 
+          Tebangan hingga Angkutan Tebangan ke TPK (Tempat Penampungan Kayu). Pendapatan KPH Kendal berasal dari penjualan kayu 
+          (jati dan rimba), produksi non kayu, kerjasama kawasan hutan (di dalam kawasan hutan dan di luar kawasan hutan), wisata 
+          dan agroforestry (jagung, kopi, pisang, jengkol dan lain-lain). Adapun kegiatan pengelolaan hutan di KPH Kendal yang meliputi 
+          tata hutan dan penyusunan rencana pengelolaan hutan, pemanfaatan hutan, rehabilitasi dan reklamasi hutan, dan 
+          perlindungan hutan dan konservasi alam.
+        </li><br />
 
-          </li><br />
-        </ul>
-        <!--Grid column-->
-      </div>
+        <li class="p fz"> Dalam pengelolaannya, KPH dibagi menjadi 6 BKPH (Bagian Kesatuan Pemangkuan 
+          Hutan) dan dibagi lagi menjadi 23 RPH (Resort Pemangkuan Hutan). Pembagian 6 BKPH dari KPH Kendal terdiri atas BKPH Mangka, 
+          BKPH Boja, BKPH Kalibodri, BKPH Sojomerto, BKPH Plelen, dan BKPH Subah.
+        </li><br />
+
+        <li class="p fz"> Batas wilayah administratif KPH Kendal adalah sebagai berikut:
+          <li class="p fz">  ->	Sebelah Utara		: Laut Jawa
+          <li class="p fz">	 -> Sebelah Timur		: Wilayah Kodya Semarang (KPH Semarang)
+          <li class="p fz">  ->	Sebelah Selatan	: Wilayah Kabupaten Temanggung (KPH Kedu Utara)
+          <li class="p fz">  -> Sebelah Barat		: Wilayah Kabupaten Batang (KPH Pekalongan Timur)
+        </li><br />
+          
+        <li class="p fz"> Luas kawasan hutan KPH Kendal jika dibagi berdasarkan fungsi kawasan terbagi menjadi 3 kawasan yakni: 
+          <li class="p fz">  ->	Kawasan untuk produksi 	       	: 15.877,56 ha (77%)
+          <li class="p fz">	 -> Kawasan perlindungan 		        : 2.688,57 ha (13%)
+          <li class="p fz">  ->	Kawasan untuk penggunaan lain 	: 2.137,15 ha (10%)
+        </li><br />
+      </ul>
+    </div>
 
 </div>
 @endsection
