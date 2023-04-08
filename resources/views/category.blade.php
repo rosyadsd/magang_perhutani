1b0cccb8e4be3cb8666d52e80a029fe7f53ee412
Muhammad Rofiul Anam <rofiulanam10@gmail.com>
2023-04-08 22:37:03 +0700

    Update category.blade.php

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -4,7 +4,8 @@
   }
 </style>
 @extends("layouts.main")
-@section("container")
+
+<section class="container-fluid py-5">
 
     <div class="container pt-5">
         <div class="row row-cols-3 pt-4"  >
@@ -13,17 +14,7 @@
           
           <div class="col " >
             <div class="card mb-3 " style="max-width: 540px; border-radius: 15px;">
-            <style>
-             .img-fluid {
-                max-width: 100%;
-                height: auto;
-              }
-              .rounded-top {
-                max-height: 300px;
-                 width: auto;
-               }
-            </style>
-            <img src="{{ $category->image_url }}" class="img-fluid rounded-top" alt="" class="img-fluid" >
+            <img src="https://source.unsplash.com/500x300?{{ $category->slug }}" class="img-fluid rounded-top" alt="" >
                  
                 <div class="card-body text-center">
                     <h5 class="card-title fs-4 d-flex justify-content-center">{{ $category->name }}</h5>
@@ -41,4 +32,4 @@
   {{ $categories->links() }}
 </div>
       
-@endsection
+</section>
