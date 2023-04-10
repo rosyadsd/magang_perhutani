f9e1f0f8a3efd309312c128caa8ebbf0cd6ffa38
Imam Nuralim <82601247+ImamNuralim@users.noreply.github.com>
2023-04-11 01:08:01 +0700

    Update main.blade.php

--- resources/views/dashboard/layouts/main.blade.php
+++ resources/views/dashboard/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="author" content="Tim KP">
-    <title>Dashboard myLearn</title>
+    <title>Dashboard</title>
     {{-- <link rel="icon" href="https://learning-aic.aerofood.co.id/Elegantic/images/favicon.png" type="image/x-icon"> --}}
     <link rel="icon" href="/img/logo-1.png" type="image/x-icon">
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
